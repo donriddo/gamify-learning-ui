648f9782d3de1b10e976512765839addaa7f547b
Ridwan Taiwo <donriddo@gmail.com>
2021-05-07 21:11:45 +0100

    extract BASE_API_URL

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,24 @@
 import Axios from 'axios';
 
+const BASE_API_URL = 'https://gamify-learning.herokuapp.com/api/';
+
 export async function fetchClasses() {
   console.log('I was called');
-  const response = await Axios.get('https://gamify-learning.herokuapp.com/api/classes');
+  const response = await Axios.get(`${BASE_API_URL}/classes`);
   console.log({ response });
   return response.data;
 }
 
 export async function fetchAssignments() {
   console.log('I was called');
-  const response = await Axios.get('https://gamify-learning.herokuapp.com/api/assignments');
+  const response = await Axios.get(`${BASE_API_URL}/assignments`);
   console.log({ response });
   return response.data;
 }
 
 export async function fetchUpcomings() {
   console.log('I was called');
-  const response = await Axios.get('https://gamify-learning.herokuapp.com/api/upcomings');
+  const response = await Axios.get(`${BASE_API_URL}/upcomings`);
   console.log({ response });
   return response.data;
 }
